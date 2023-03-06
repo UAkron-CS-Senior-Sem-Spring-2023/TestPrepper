1493f0d0b3924d25ffdc3698a6cb14148371aa96
mdudek0802 <mcd81@uakron.edu>
2023-03-06 18:32:37 -0500

    Remove old comments and update with new ones

--- game/opencv_example.py
+++ game/opencv_example.py
@@ -1,16 +1,7 @@
-from pygame import camera
 import pygame
-import time
 import cv2 as cv
-import os
 import numpy as np
 
-# from yunet import YuNet
-
-# Recognition
-# HAAR_PATH = "/usr/share/opencv/haarcascades/"
-
-FACE_CASCADE = cv.CascadeClassifier('haarcascade_frontalcatface.xml')
 FACE_DETECTOR = cv.FaceDetectorYN.create(
         'face_detection_yunet_2022mar.onnx',
         "",
@@ -20,39 +11,8 @@ FACE_DETECTOR = cv.FaceDetectorYN.create(
         5000
     )
 
-# # Face
-# # FACE_HAAR = os.path.join(HAAR_PATH, "haarcascade_frontalface_default.xml")
-# FACE_HAAR = cv.Load(FACE_HAAR)
-
-# # Eye
-# EYE_HAAR = os.path.join(HAAR_PATH, "haarcascade_mcs_righteye.xml")
-# EYE_HAAR = cv.Load(EYE_HAAR)
-
-# # Nose
-# NOSE_HAAR = os.path.join(HAAR_PATH, "haarcascade_mcs_nose.xml")
-# NOSE_HAAR = cv.Load(NOSE_HAAR)
-
-# # Mouth
-# MOUTH_HAAR = os.path.join(HAAR_PATH, "haarcascade_mcs_mouth.xml")
-# MOUTH_HAAR = cv.Load(MOUTH_HAAR)
-
 # Screen settings
 SCREEN = [1200, 800]
-
-
-# def surface_to_string(surface):
-#     """Convert a pygame surface into string"""
-#     return pygame.image.tostring(surface, 'RGB')
-
-
-# def pygame_to_cvimage(surface):
-#     """Convert a pygame surface into a cv image"""
-#     cv_image = cv.CreateImageHeader(surface.get_size(), cv.IPL_DEPTH_8U, 3)
-#     image_string = surface_to_string(surface)
-#     cv.SetData(cv_image, image_string)
-#     return pygame.image.frombuffer(image.tostring(), image.shape[:2],
-#                                    "RGB")
-
 
 def cvimage_grayscale(cv_image):
     """Converts a cvimage into grayscale"""
@@ -66,37 +26,14 @@ def cvimage_to_pygame(image):
 
 
 def detect_faces(cv_image):
-    """Detects faces based on haar. Returns points"""
-    # return FACE_CASCADE.detectMultiScale(cvimage_grayscale(cv_image))
+    """Detects faces. Returns list of faces if any and their points"""
     return FACE_DETECTOR.detect(cv_image)
-    # return cv.HaarDetectObjects(cvimage_grayscale(cv_image), FACE_HAAR,
-    #                             storage)
-
-
-# def detect_eyes(cv_image, storage):
-#     """Detects eyes based on haar. Returns points"""
-#     return cv.HaarDetectObjects(cvimage_grayscale(cv_image), EYE_HAAR,
-#                                 storage)
-
-
-# def detect_nose(cv_image, storage):
-#     """Detects nose based on haar. Returns ponts"""
-#     return cv.HaarDetectObjects(cvimage_grayscale(cv_image), NOSE_HAAR,
-#                                 storage)
-
-
-# def detect_mouth(cv_image, storage):
-#     """Detects mouth based on haar. Returns points"""
-#     return cv.HaarDetectObjects(cvimage_grayscale(cv_image), MOUTH_HAAR,
-#                                 storage)
 
 
 def draw_from_points(cv_image, points):
     """Takes the cv_image and points and draws a rectangle based on the points.
+    Also draws points on where it thinks the eyes, nose, and mouth corners are.
     Returns a cv_image."""
-    # for (x, y, w, h) in points:
-    #     cv.rectangle(cv_image, (x, y), (x + w, y + h), 255)
-    # return cv_image
     if points[1] is not None:
         for idx, face in enumerate(points[1]):
             print('Face {}, top-left coordinates: ({:.0f}, {:.0f}), box width: {:.0f}, box height {:.0f}, score: {:.2f}'.format(idx, face[0], face[1], face[2], face[3], face[-1]))
@@ -111,95 +48,53 @@ def draw_from_points(cv_image, points):
     
     return cv_image
 
-
-# if __name__ == '__main__':
-
-#     # Set game screen
-#     screen = pygame.display.set_mode(SCREEN)
-
-#     pygame.init()  # Initialize pygame
-#     camera.init()  # Initialize camera
-
-#     # Load camera source then start
-#     cam = camera.Camera('/dev/video0', SCREEN)
-#     cam.start()
-
-#     while 1:  # Ze loop
-
-#         time.sleep(1 / 120)  # 60 frames per second
-
-#         image = cam.get_image()  # Get current webcam image
-
-#         cv_image = pygame_to_cvimage(image)  # Create cv image from pygame image
-
-#         # Detect faces then draw points on image
-#         # FIXME: Current bottleneck. Image has to be Grayscale to make it faster.
-#         #        One solution would be to use opencv instead of pygame for
-#         #        capturing images.
-#         # storage = cv.CreateMemStorage(-1)  # Create storage
-#         #points = detect_eyes(cv_image, storage) + \
-#         #        detect_nose(cv_image, storage) + \
-#         #        detect_mouth(cv_image, storage)
-#         points = detect_faces(cv_image)  # Get points of faces.
-#         cv_image = draw_from_points(cv_image, points)  # Draw points
-
-#         screen.fill([0, 0, 0])  # Blank fill the screen
-
-#         screen.blit(cvimage_to_pygame(cv_image), (0, 0))  # Load new image on screen
-
-#         pygame.display.update()  # Update pygame display
-
 if __name__ == '__main__':
 
     # Set game screen
     screen = pygame.display.set_mode(SCREEN)
 
     pygame.init()  # Initialize pygame
-    # camera.init()  # Initialize camera
 
     # Load camera source then start
     cap = cv.VideoCapture(0)
-    # cam = camera.Camera('/dev/video0', SCREEN)
-    # cam.start()
 
+    # Variable to control loop exit
     game_exit = False
 
+    # Get the start time of the game
     start_time = pygame.time.get_ticks()
+    now = start_time
 
-    while not game_exit:  # Ze loop
-
+    # Main Loop
+    while not game_exit:
+        # Exit the game if it was told to quit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_exit = True
 
-        # time.sleep(1 / 120)  # 60 frames per second
-        now = pygame.time.get_ticks()
+        # Get current time and the elapsed time since start
         elapsed = now - start_time
-
-        if elapsed > 16.7:
-            ret, image = cap.read()  # Get current webcam image
-
+        now = pygame.time.get_ticks()
+        
+        # If its been more than 16.7 ms, roughly 60 fps
+        if elapsed >= 16.7:
+            # Get current webcam image
+            ret, image = cap.read()
+            # Tell the face detector what size image to expect
             FACE_DETECTOR.setInputSize((image.shape[1], image.shape[0]))
 
             if ret:
-                # cv_image = pygame_to_cvimage(image)  # Create cv image from pygame image
-
-                # Detect faces then draw points on image
-                # FIXME: Current bottleneck. Image has to be Grayscale to make it faster.
-                #        One solution would be to use opencv instead of pygame for
-                #        capturing images.
-                # storage = cv.CreateMemStorage(-1)  # Create storage
-                #points = detect_eyes(cv_image, storage) + \
-                #        detect_nose(cv_image, storage) + \
-                #        detect_mouth(cv_image, storage)
-                faces = detect_faces(image)  # Get points of faces.
-                print(faces)
-                image = draw_from_points(image, faces)  # Draw points
-
-                screen.fill([0, 0, 0])  # Blank fill the screen
-
-                screen.blit(cvimage_to_pygame(image), (0, 0))  # Load new image on screen
-
-                pygame.display.update()  # Update pygame display
-
+                # Get all faces in image
+                faces = detect_faces(image)
+                # for debug, prints out the faces
+                # print(faces)
+                # Draws rectangles on the faces, returns the edited image
+                image = draw_from_points(image, faces)
+                # Blank fill the screen
+                screen.fill([0, 0, 0])
+                # Load new image on screen
+                screen.blit(cvimage_to_pygame(image), (0, 0))
+                # Update pygame display
+                pygame.display.update()
+    # Quit pygame
     pygame.quit()
