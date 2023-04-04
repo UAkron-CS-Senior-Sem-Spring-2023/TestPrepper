8a964995c2abc0bb8edf64d00977149cbf5480f9
mdudek0802 <mcd81@uakron.edu>
2023-04-03 20:02:04 -0400

    Show y of highest hand point on image

--- game/gesture_detection.py
+++ game/gesture_detection.py
@@ -15,9 +15,11 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 MODEL = 'gesture_recognizer.task'
 MODEL_CONTENTS = open(MODEL, 'rb').read()
-
-def main():
-    pass
+FONT_COLOR = (255,255,255)
+POINTING_UP_QUOTE = "You're pointing up! At: "
+FONT = cv.FONT_HERSHEY_COMPLEX
+FONT_SCALE = 0.5
+THICKNESS = 1
 
 if __name__ == "__main__":
     cap = cv.VideoCapture(0)
@@ -35,15 +37,26 @@ if __name__ == "__main__":
 
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.asarray(image))
             gesture_recognition_result = recognizer.recognize_for_video(mp_image, mp.Timestamp.from_seconds(time.time()).microseconds())
+
+            top_finger_x = -1
+            top_finger_y = 10000
             if len(gesture_recognition_result.gestures) > 0:
                 for i in range(len(gesture_recognition_result.gestures)):
                     if gesture_recognition_result.gestures[i][0].category_name == 'Pointing_Up':
-                        print("You're pointing up!")
+                        # print("You're pointing up!")
                         for mark in gesture_recognition_result.hand_landmarks[0]:
                             x = int(mark.x * image.shape[1])
                             y = int(mark.y * image.shape[0])
                             # Annotate landmarks
                             cv.circle(image, (x, y), 5, (0, 255, 0), -1)
+
+                            if top_finger_y > y:
+                                top_finger_y = y
+                                top_finger_x = x
+
+                        pointing_len = cv.getTextSize(POINTING_UP_QUOTE, FONT, FONT_SCALE, 1)
+                        cv.putText(image, POINTING_UP_QUOTE, (top_finger_x, top_finger_y), FONT, FONT_SCALE, FONT_COLOR, THICKNESS, cv.LINE_AA)
+                        cv.putText(image, str(top_finger_y), (top_finger_x+pointing_len[0][0], top_finger_y), FONT, FONT_SCALE, FONT_COLOR, THICKNESS, cv.LINE_AA)
 
             cv.imshow('img', image)
             key = cv.waitKey(1)
