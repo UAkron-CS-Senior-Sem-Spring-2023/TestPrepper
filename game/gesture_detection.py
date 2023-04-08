01392d21965f10218931479e7bb45e85e15b89ab
mdudek0802 <mcd81@uakron.edu>
2023-04-08 19:46:12 -0400

    Classify the gesture detector

--- game/gesture_detection.py
+++ game/gesture_detection.py
@@ -21,6 +21,59 @@ FONT = cv.FONT_HERSHEY_COMPLEX
 FONT_SCALE = 0.5
 THICKNESS = 1
 
+class GestureDetection():
+    def __init__(
+            self,
+            camera_num:int
+        ):
+        
+        self.cam = cv.VideoCapture(int(camera_num))
+        options = GestureRecognizerOptions(
+            base_options=BaseOptions(model_asset_buffer=MODEL_CONTENTS),
+            running_mode=VisionRunningMode.VIDEO
+        )
+        self.recognizer = GestureRecognizer.create_from_options(options)
+        self.drawing = mp.solutions.drawing_utils
+
+    def __del__(self):
+        del self.recognizer
+
+    def getImage(self):
+        ret, image = self.cam.read()
+        assert(ret)
+        return image
+
+    def doesImageContainGesture(
+        self,
+        gesture: str,
+        image
+    ):
+        
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.asarray(image))
+        gesture_recognition_result = self.recognizer.recognize_for_video(mp_image, mp.Timestamp.from_seconds(time.time()).microseconds())
+
+        top_finger_x = -1
+        top_finger_y = 10000
+        if len(gesture_recognition_result.gestures) > 0:
+            for i in range(len(gesture_recognition_result.gestures)):
+                if gesture_recognition_result.gestures[i][0].category_name == gesture:
+                    # print("You're pointing up!")
+                    for mark in gesture_recognition_result.hand_landmarks[0]:
+                        x = int(mark.x * image.shape[1])
+                        y = int(mark.y * image.shape[0])
+                        # Annotate landmarks
+                        cv.circle(image, (x, y), 5, (0, 255, 0), -1)
+
+                        if top_finger_y > y:
+                            top_finger_y = y
+                            top_finger_x = x
+
+                    pointing_len = cv.getTextSize(POINTING_UP_QUOTE, FONT, FONT_SCALE, 1)
+                    cv.putText(image, POINTING_UP_QUOTE, (top_finger_x, top_finger_y), FONT, FONT_SCALE, FONT_COLOR, THICKNESS, cv.LINE_AA)
+                    cv.putText(image, str(top_finger_y), (top_finger_x+pointing_len[0][0], top_finger_y), FONT, FONT_SCALE, FONT_COLOR, THICKNESS, cv.LINE_AA)
+                return (image, (top_finger_x, top_finger_y))
+        return False
+
 if __name__ == "__main__":
     cap = cv.VideoCapture(0)
     # Create a gesture recognizer instance with the video mode:
