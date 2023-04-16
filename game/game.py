c9e48c87b48c7e01f1ab4c01bd041fbaac1e234a
mdudek0802 <mcd81@uakron.edu>
2023-04-16 15:29:55 -0400

    Get rid of unused code and debug code

--- game/game.py
+++ game/game.py
@@ -39,11 +39,6 @@ TOP_ANSWER_END = TOP_ANSWER_START+ANSWER_HEIGHT
 BOTTOM_ANSWER_START = CV_SCREEN_OFFSET+(3*ANSWER_USABLE_SPACE/4)-(ANSWER_HEIGHT/2)
 BOTTOM_ANSWER_END = BOTTOM_ANSWER_START+ANSWER_HEIGHT
 
-print(RIGHT_ANSWER_START)
-print(RIGHT_ANSWER_END)
-print(BOTTOM_ANSWER_START)
-print(BOTTOM_ANSWER_END)
-
 def cvimage_to_pygame(image):
     """Convert cvimage into a pygame image"""
     return pygame.image.frombuffer(image.tobytes(), image.shape[1::-1], "BGR")
@@ -145,12 +140,6 @@ def game(chapterName, termList, definitionList):
                 choicePicker = [termList[x],termList[random.choice(array)],termList[random.choice(array)],termList[random.choice(array)]]
         random.shuffle(choicePicker)
 
-        # questionChoice = termList[x]
-        # print("{}".format(definitionList[x]) + "?")
-        # print("A.{}".format(choicePicker[0]))
-        # print("B.{}".format(choicePicker[1]))
-        # print("C.{}".format(choicePicker[2]))
-        # print("D.{}".format(choicePicker[3]))
         answer = quiz(
             definitionList[x],
             choicePicker[0],
@@ -174,35 +163,5 @@ if __name__ == "__main__":
         ['CPU', 'RAM', 'SSD', 'Motherboard', 'Case', 'GPU'],
         ['Processes all the instructions fed in by RAM', 'Volatile storage for quick access fed in by SSD', 'Large, slow storage to hold information', 'Electronic housing for all the components', 'Physical housing for all the components', 'Powers what you see on the display']
         )
-    input()
     exit(0)
     
-
-    game_exit = False
-
-    while not game_exit:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_exit = True
-
-        image = detecter.getImage()
-
-        result = detecter.doesImageContainGesture('Open_Palm', image)
-
-        # image = np.zeros([image.shape[0],image.shape[1],3],dtype=np.uint8)
-        # image.fill(255)
-
-        # screen.fill([0,0,0])
-        # screen.blit(cvimage_to_pygame(image), (0,50))
-        pygame.draw.rect(screen, (255,255,255), (0,0,WIDTH,HEIGHT))
-        
-        if result != False:
-            x, y = getXandYCoords(image.shape[1], image.shape[0], result[1])
-            pygame.draw.circle(screen, (0,0,0), (image.shape[1]-x,y+CV_SCREEN_OFFSET), radius=10)
-
-        pygame.display.update()
-        clock.tick(5)
-    
-    pygame.quit()
-    quit()
-
