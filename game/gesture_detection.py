07067b6e8fd03bbc940bb0b9799eecb0f3e68cf2
mdudek0802 <mcd81@uakron.edu>
2023-04-16 16:57:57 -0400

    Comment out old example code

--- game/gesture_detection.py
+++ game/gesture_detection.py
@@ -87,45 +87,45 @@ def getXandYCoords(width, height, landmarks):
     return (top_finger_x, top_finger_y)
 
 
-if __name__ == "__main__":
-    cap = cv.VideoCapture(0)
-    # Create a gesture recognizer instance with the video mode:
-    options = GestureRecognizerOptions(
-        base_options=BaseOptions(model_asset_buffer=MODEL_CONTENTS),
-        running_mode=VisionRunningMode.VIDEO
-    )
-    mp_drawing = mp.solutions.drawing_utils
-    with GestureRecognizer.create_from_options(options) as recognizer:
-        while True:
-            ret, image = cap.read()
-            if not ret:
-                exit(-1)
+# if __name__ == "__main__":
+#     cap = cv.VideoCapture(0)
+#     # Create a gesture recognizer instance with the video mode:
+#     options = GestureRecognizerOptions(
+#         base_options=BaseOptions(model_asset_buffer=MODEL_CONTENTS),
+#         running_mode=VisionRunningMode.VIDEO
+#     )
+#     mp_drawing = mp.solutions.drawing_utils
+#     with GestureRecognizer.create_from_options(options) as recognizer:
+#         while True:
+#             ret, image = cap.read()
+#             if not ret:
+#                 exit(-1)
 
-            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.asarray(image))
-            gesture_recognition_result = recognizer.recognize_for_video(mp_image, mp.Timestamp.from_seconds(time.time()).microseconds())
+#             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=np.asarray(image))
+#             gesture_recognition_result = recognizer.recognize_for_video(mp_image, mp.Timestamp.from_seconds(time.time()).microseconds())
 
-            top_finger_x = -1
-            top_finger_y = 10000
-            if len(gesture_recognition_result.gestures) > 0:
-                for i in range(len(gesture_recognition_result.gestures)):
-                    if gesture_recognition_result.gestures[i][0].category_name == 'Pointing_Up':
-                        # print("You're pointing up!")
-                        for mark in gesture_recognition_result.hand_landmarks[0]:
-                            x = int(mark.x * image.shape[1])
-                            y = int(mark.y * image.shape[0])
-                            # Annotate landmarks
-                            cv.circle(image, (x, y), 5, (0, 255, 0), -1)
+#             top_finger_x = -1
+#             top_finger_y = 10000
+#             if len(gesture_recognition_result.gestures) > 0:
+#                 for i in range(len(gesture_recognition_result.gestures)):
+#                     if gesture_recognition_result.gestures[i][0].category_name == 'Pointing_Up':
+#                         # print("You're pointing up!")
+#                         for mark in gesture_recognition_result.hand_landmarks[0]:
+#                             x = int(mark.x * image.shape[1])
+#                             y = int(mark.y * image.shape[0])
+#                             # Annotate landmarks
+#                             cv.circle(image, (x, y), 5, (0, 255, 0), -1)
 
-                            if top_finger_y > y:
-                                top_finger_y = y
-                                top_finger_x = x
+#                             if top_finger_y > y:
+#                                 top_finger_y = y
+#                                 top_finger_x = x
 
-                        pointing_len = cv.getTextSize(POINTING_UP_QUOTE, FONT, FONT_SCALE, 1)
-                        cv.putText(image, POINTING_UP_QUOTE, (top_finger_x, top_finger_y), FONT, FONT_SCALE, FONT_COLOR, THICKNESS, cv.LINE_AA)
-                        cv.putText(image, str(top_finger_y), (top_finger_x+pointing_len[0][0], top_finger_y), FONT, FONT_SCALE, FONT_COLOR, THICKNESS, cv.LINE_AA)
+#                         pointing_len = cv.getTextSize(POINTING_UP_QUOTE, FONT, FONT_SCALE, 1)
+#                         cv.putText(image, POINTING_UP_QUOTE, (top_finger_x, top_finger_y), FONT, FONT_SCALE, FONT_COLOR, THICKNESS, cv.LINE_AA)
+#                         cv.putText(image, str(top_finger_y), (top_finger_x+pointing_len[0][0], top_finger_y), FONT, FONT_SCALE, FONT_COLOR, THICKNESS, cv.LINE_AA)
 
-            cv.imshow('img', image)
-            key = cv.waitKey(1)
-            if key == ord('q'):
-                exit(1)
+#             cv.imshow('img', image)
+#             key = cv.waitKey(1)
+#             if key == ord('q'):
+#                 exit(1)
                         
